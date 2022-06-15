19e5752e9b72835591476beeae22cdd5b4b0f768
ktKongTong <44502608+ktKongTong@users.noreply.github.com>
2022-06-15 14:13:09 +0800

    test

--- batch-spot/main.tf
+++ batch-spot/main.tf
@@ -1,74 +1,42 @@
-variable "profile" {
-  default = "default"
-}
-variable "region" {
-  default = "cn-hangzhou"
-}
 provider "alicloud" {
-  region  = var.region
-  profile = var.profile
+    region  = "cn-beijing"
 }
 
-variable "instances_number" {
-  default = 2
+data "alicloud_vswitches" "default" {
+  zone_id = "cn-beijing"
+  vswitch_name = "slb-test"
+}
+data "alicloud_security_groups" "default" {
+  name_regex  = "slb-test"
 }
 
-##################################################################
-# Data sources to get VPC, subnet, security group and AMI details
-##################################################################
-data "alicloud_vpcs" "default" {
-  is_default = true
+data "alicloud_instance_types" "default" {
+  // 可用区
+  availability_zone = data.alicloud_vswitches.default.vswitches.0.zone_id
+  cpu_core_count = 1
+  memory_size    = 2
 }
-
-data "alicloud_vswitches" "all" {
-  vpc_id = data.alicloud_vpcs.default.ids.0
-}
-
-data "alicloud_images" "ubuntu" {
-  most_recent = true
-  name_regex  = "^ubuntu_18.*64"
-}
-
-// retrieve 1c2g instance type
-data "alicloud_instance_types" "normal" {
-  availability_zone = data.alicloud_vswitches.all.vswitches.0.zone_id
-  cpu_core_count    = 1
-  memory_size       = 2
-}
-
-// Security Group module for ECS Module
-module "security_group" {
-  source  = "alibaba/security-group/alicloud"
-  profile = var.profile
-  region  = var.region
-  vpc_id  = data.alicloud_vpcs.default.ids.0
-  version = "~> 2.0"
-}
-
-module "ecs" {
-  source  = "../.."
-  profile = var.profile
-  region  = var.region
-
-  number_of_instances = var.instances_number
-
-  name                        = "example-with-eips"
-  image_id                    = data.alicloud_images.ubuntu.ids.0
-  instance_type               = data.alicloud_instance_types.normal.ids.0
-  vswitch_id                  = data.alicloud_vswitches.all.ids.0
-  security_group_ids          = [module.security_group.this_security_group_id]
-  associate_public_ip_address = false
-}
-
-resource "alicloud_eip_association" "this_ecs" {
-  count = var.instances_number
-
-  instance_id   = module.ecs.this_instance_id[count.index]
-  allocation_id = alicloud_eip.this[count.index].id
-}
-
-resource "alicloud_eip" "this" {
-  count = var.instances_number
-
-  bandwidth = 10
+# ECS
+resource "alicloud_instance" "instance" {
+    # 数量
+    count = var.instance_number
+    // 实例类型
+    instance_type = data.alicloud_instance_types.default.ids.0
+    // 安全组
+    security_groups  = data.alicloud_security_groups.default.groups.0.id
+    # 交换机id 
+    vswitch_id = data.alicloud_vswitches.default.vswitches.0.id
+    # 镜像id 
+    image_id = var.image_id
+    # 实例名称 
+    instance_name = "example-with-eips"
+    # 计费方式，后付费
+    instance_charge_type = "PostPaid"
+    # 计费策略，按量付费，竞价抢占
+    spot_strategy ="SpotAsPriceGo"
+    # 峰值带宽
+    internet_max_bandwidth_out = 0
+    # 系统盘类型
+    system_disk_category  = "cloud_efficiency"
+    system_disk_name   = "test_ecs_disk"
 }
---
+++ batch-spot/slb/main.tf
@@ -0,0 +1,50 @@
+provider "alicloud" {
+    region  = "cn-beijing"
+}
+# 安全组
+resource "alicloud_security_group" "group" {
+    name = "slb-test"
+    vpc_id = alicloud_vpc.vpc.id
+}
+# 安全组规则
+resource "alicloud_security_group_rule" "allow_all_tcp" {
+  type              = "ingress"
+  ip_protocol       = "tcp"
+  nic_type          = "intranet"
+  policy            = "accept"
+  port_range        = "1/65535"
+  priority          = 1
+  security_group_id = alicloud_security_group.group.id
+  cidr_ip           = "0.0.0.0/0"
+}
+
+# 专用网络
+resource "alicloud_vpc" "vpc" {
+    vpc_name = "slb-test"
+    cidr_block = "172.16.0.0/16"
+}
+# 交换机
+resource "alicloud_vswitch" "vswitch" {
+    vpc_id = alicloud_vpc.vpc.id
+    cidr_block = "172.16.0.0/24"
+    zone_id = "cn-beijing"
+    vswitch_name = "slb-test"
+}
+#创建一个负载均衡实例（传统型）
+resource "alicloud_slb_load_balancer" "default" {
+  load_balancer_name = "spot-loadbalancer-test"
+  // 私网，之后绑定EIP
+  address_type       = "intranet"
+  load_balancer_spec = "slb.s1.small"
+  // 交换机
+  vswitch_id         = alicloud_vswitch.default.id
+}
+
+// 使用弹性IP
+resource "alicloud_eip_address" "eip" {
+}
+// 关联弹性IP到SLB
+resource "alicloud_eip_association" "eip_asso" {
+  allocation_id = alicloud_eip_address.eip.id
+  instance_id = alicloud_slb_load_balancer.default.id
+}
